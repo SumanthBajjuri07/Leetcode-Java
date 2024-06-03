f50aa737fc65fcadb96b3093ce8e7c5c3a3788e9
Sumanth Bajjuri <73301202+SumanthBajjuri07@users.noreply.github.com>
2024-06-03 17:19:56 +0530

    Create 0389.java

---
+++ 0389.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        
+        int xor = 0;
+        for(int i=0; i<s.length(); i++){
+            xor = xor ^ (int) s.charAt(i);
+        }
+
+        for(int i=0; i<t.length(); i++){
+            xor = xor ^ (int) t.charAt(i);
+        }
+
+        return (char)xor;
+
+
+
+    }
+}
