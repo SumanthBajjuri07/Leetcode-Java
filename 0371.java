f880090a834809d96e5f2a9f6bc57d779725c427
Sumanth Bajjuri <73301202+SumanthBajjuri07@users.noreply.github.com>
2024-06-08 02:04:49 +0530

    Create 0371.java

---
+++ 0371.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int getSum(int a, int b) {
+     
+        while(b!=0)
+        {
+            int temp=a;
+            a=a^b;
+            b=(b&temp)<<1;
+        }
+        return a;
+    }
+}
